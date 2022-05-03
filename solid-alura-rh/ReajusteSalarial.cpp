75bc4bbfa65585608d1e9ac52f591589b3562617
Gabriel Almeida <gabrielcrta@gmail.com>
2022-05-03 20:22:38 -0300

    Adiciona nova validação

--- solid-alura-rh/ReajusteSalarial.cpp
+++ solid-alura-rh/ReajusteSalarial.cpp
@@ -1,13 +1,33 @@
+#include <chrono>
 #include "ReajusteSalarial.hpp"
 #include "ValidacaoException.hpp"
 
 void reajustaSalarioDeFuncionario(Funcionario& funcionario, double aumento)
 {
-	const auto salarioAtual = funcionario.getSalario();
-	const auto percentualReajuste = aumento / salarioAtual;
+	validaPercentualDeReajuste(funcionario, aumento);
+	validaIntervaloEntreReajustes(funcionario);
+
+	const auto novoSalario = funcionario.getSalario() + aumento;
+	funcionario.reajustarSalario(novoSalario);
+}
+
+void validaPercentualDeReajuste(const Funcionario& funcionario, double aumento)
+{
+	const auto percentualReajuste = aumento / funcionario.getSalario();
 	if (percentualReajuste > 0.4) {
 		throw ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
 	}
-	const auto novoSalario = salarioAtual + aumento;
-	funcionario.reajustarSalario(novoSalario);
+}
+
+void validaIntervaloEntreReajustes(const Funcionario& funcionario)
+{
+	// conforme definido em https://en.cppreference.com/w/cpp/chrono/duration
+	using Meses = std::chrono::duration<int, std::ratio<2629746>>;
+
+	const auto dataAtual = std::chrono::system_clock::now();
+	const auto dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+	const auto diferencaEmMeses = std::chrono::duration_cast<Meses>(dataAtual - dataUltimoReajuste).count();
+	if (diferencaEmMeses < 6) {
+		throw ValidacaoException("Menos de 6 meses entre reajustes!");
+	}
 }
--- solid-alura-rh/ReajusteSalarial.hpp
+++ solid-alura-rh/ReajusteSalarial.hpp
@@ -2,3 +2,5 @@
 #include "Funcionario.hpp"
 
 void reajustaSalarioDeFuncionario(Funcionario& funcionario, double novoSalario);
+void validaPercentualDeReajuste(const Funcionario& funcionario, double aumento);
+void validaIntervaloEntreReajustes(const Funcionario& funcionario);
